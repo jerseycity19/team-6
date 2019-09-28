0201d79a3441aeda73fdade629a40a45777f4cd5
Edden Kashi <ekashi2@pride.hofstra.edu>
2019-09-27 21:36:57 -0400

    chat bot feature

---
+++ chatbot.py
@@ -0,0 +1,26 @@
+#import necessary libraries
+import io
+import random
+import string # to process standard python strings
+import warnings
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import warnings
+warnings.filterwarnings('ignore')
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+# Keyword Matching
+GREETING_INPUTS = ("hello")
+GREETING_RESPONSES = ["hi"]
+
+def greeting(sentence):
+    """If user's input is a greeting, return a greeting response"""
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+        
+
