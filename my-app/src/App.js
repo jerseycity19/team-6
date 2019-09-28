6b3a2ae26d20e7a512ba0eda41353f09c21cc18b
Peter <peterdraczkowski@gmail.com>
2019-09-27 23:18:39 -0700

    leaflet map just added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,12 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <div className="navbar">
-          navbar goes here
-        </div>
-        <div className="Testimonials">
-          testimonials go here
-        </div>
         <div id="mapid">
         </div>
     </div>
