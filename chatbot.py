295925300eed40d8338815ab6cb9c634c0889d7a
Edden Kashi <ekashi2@pride.hofstra.edu>
2019-09-28 00:05:49 -0400

    chat bot

--- chatbot.py
+++ chatbot.py
@@ -4,8 +4,8 @@ import random
 import string # to process standard python strings
 import warnings
 import numpy as np
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+#from sklearn.feature_extraction.text import TfidfVectorizer
+#from sklearn.metrics.pairwise import cosine_similarity
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -22,5 +22,42 @@ def greeting(sentence):
         if word.lower() in GREETING_INPUTS:
             return random.choice(GREETING_RESPONSES)
 
+
+##commit 2
+
+# Generating response
+def response(user_response):
+    robo_response=''
+
+
+#switch function
+def switch(user_response):
+    print("user response: ", user_response)
+    switcher = {
+        "1": "addiction link",
+        "2": "prevention link",
+        "3": "treatment link",
+        "4": "recovery link",
+    }
+    print(switcher[user_response])
+    print("Do you want to continue:")
+    repeat = input()
+    print(repeat)
+
+#print switch.get(user_response, "Invalid option")
+
+
+flag=True
+repeat = "yes"
+print("Are you in need of immediate help?")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    #to exit
+
+    else:
+        flag=False
+        print("ROBO: Bye! take care..")    
         
+
 
