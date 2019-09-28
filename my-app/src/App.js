829ef000fcace18a4cab668a5e329f3421a3bc45
ckhorram <khorramcyrus@gmail.com>
2019-09-28 03:04:53 -0400

    comm

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Homepage';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
-<<<<<<< HEAD
     <div>
           <HomePage/>
-=======
-    <div className="App">
-        <div id="mapid">
-        </div>
->>>>>>> master
+          <div className="App">
+          <div id="mapid">
+          </div>
+          </div>
     </div>
   );
 }
